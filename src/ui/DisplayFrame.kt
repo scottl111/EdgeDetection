5a56eca071db9a6707d392a1b5ead7aed1016d58
Scott <Scott>
2018-12-18 01:01:27 +0000

    Added the Jmenu drop down

--- src/ui/DisplayFrame.kt
+++ src/ui/DisplayFrame.kt
@@ -1,9 +1,6 @@
 package ui
 
-import java.awt.BorderLayout
-import java.awt.Color
-import java.awt.Dimension
-import java.awt.GridLayout
+import java.awt.*
 import javax.swing.*
 
 /**
@@ -11,7 +8,7 @@ import javax.swing.*
  */
 class DisplayFrame(private val thresholdOneSlider: JSlider = JSlider(),
                    private val thresholdTwoSlider: JSlider = JSlider(),
-                   private val kernelComboBox: JComboBox<Int> = JComboBox(arrayOf(3, 9)),
+                   private val kernelComboBox: JComboBox<Int> = JComboBox(arrayOf(3, 5, 9, 12)),
                    private var thresholdOneValue: Int = 50,
                    private var thresholdTwoValue: Int = 150,
                    private var kernelSize: Int = 3) : JFrame() {
@@ -23,10 +20,10 @@ class DisplayFrame(private val thresholdOneSlider: JSlider = JSlider(),
         val height = 500
         val width = 1000
         val frameSize = Dimension(width, height)
-        val displayPanelSize = Dimension(400,500)
-        val adjustmentPanelSize = Dimension(100, 500)
 
-        // Set up the adjustment panel
+        // ============================
+        // ADJUSTMENT PANEL
+        // ============================
         val adjustmentPanel = JPanel(GridLayout(2, 3, 5, 5))
         topLevelPanel.add(adjustmentPanel, BorderLayout.NORTH)
         adjustmentPanel.background = Color.YELLOW
@@ -39,16 +36,31 @@ class DisplayFrame(private val thresholdOneSlider: JSlider = JSlider(),
         adjustmentPanel.add(kernelComboBox)
         adjustmentPanel.add(thresholdTwoSlider)
 
-        // Set up the display panel
+        // ============================
+        // DISPLAY PANEL
+        // ============================
         val displayPanel = JPanel()
         topLevelPanel.add(displayPanel, BorderLayout.CENTER)
         displayPanel.background = Color.BLUE
 
-        // Set up the frame
+        // ============================
+        // FRAME
+        // ============================
         size = frameSize
         isVisible = true
         defaultCloseOperation = JFrame.EXIT_ON_CLOSE;
         title = "Edge Detection"
+
+        val operationsMenuBar = JMenuBar()
+        val fileMenu = JMenu("File")
+        operationsMenuBar.add(fileMenu)
+        val saveMenuItem = JMenuItem("Save Image")
+        val loadMenuItem = JMenuItem("Load Image")
+        fileMenu.add(saveMenuItem)
+        fileMenu.add(loadMenuItem)
+        jMenuBar = operationsMenuBar
+        validate()
+        repaint()
     }
 
 }
