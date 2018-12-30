3dbb1e2d50abca1da1aca4a732dada460da4d0f2
Scott Lockett <scottl111@hotmail.co.uk>
2018-12-30 22:14:00 +0000

    Fixed compilation issue on the utilites package

--- src/main/kotlin/edgeDetectionAlgorithm/EdgeDetectionOperator.kt
+++ src/main/kotlin/edgeDetectionAlgorithm/EdgeDetectionOperator.kt
@@ -5,7 +5,7 @@ import org.opencv.core.CvType
 import org.opencv.core.Mat
 import org.opencv.imgproc.Imgproc
 import main.kotlin.ui.DisplayImage
-import utility.ImageUtils
+import main.kotlin.utility.ImageUtilities
 
 /**
  * Class for applying edge detection.
@@ -34,7 +34,7 @@ class EdgeDetectionOperator
         //TODO use logger here as trace level
         System.err.println("first threshold: $thresholdOne second threshold: $thresholdTwo kernel size: $kernelSize")
 
-        val colourMat = ImageUtils.convertImageToMatrix(img)
+        val colourMat = ImageUtilities.convertImageToMatrix(img)
         val grey = Mat()
         val draw = Mat()
         val wide = Mat()
@@ -43,6 +43,6 @@ class EdgeDetectionOperator
         Imgproc.Canny(grey, wide, 50.0 + thresholdOne, 100.0 + thresholdTwo, kernelSize, false)
         wide.convertTo(draw, CvType.CV_8U)
 
-        DisplayImage.edgeDetectedImage = ImageUtils.convertMatrixToImage(draw)
+        DisplayImage.edgeDetectedImage = ImageUtilities.convertMatrixToImage(draw)
     }
 }
